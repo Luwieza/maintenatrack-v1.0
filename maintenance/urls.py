287f365702eb82f9ff671efb48449b02d33a0df8
Luxolo Gqadu <luwie@Luxolos-MacBook-Air.local>
2025-09-06 21:10:14 +0100

    App URLS routed

---
+++ maintenance/urls.py
@@ -0,0 +1,11 @@
+# maintenance/urls.py
+from django.urls import path
+from . import views
+
+app_name = "maintenance"
+
+urlpatterns = [
+    path("", views.log_list, name="log_list"),
+    path("logs/new/", views.log_create, name="log_create"),
+    path("logs/<int:pk>/", views.log_detail, name="log_detail"),
+]
